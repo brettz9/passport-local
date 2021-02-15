f10b61c52ce8d7d1bd007e80275daa4019740219
Brett Zamir <brettz9@yahoo.com>
2021-02-15 12:25:51 +0800

    - Linting: Use ESLint 7 new processor API as now targeted by `eslint-plugin-markdown` and `eslint-plugin-jsdoc` - npm: Update devDeps.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,10 @@ module.exports = {
     },
     {
       files: ['**/*.md'],
+      processor: 'markdown/markdown'
+    },
+    {
+      files: '**/*.md/*.js',
       rules: {
         'eol-last': ['off'],
         'no-console': ['off'],
